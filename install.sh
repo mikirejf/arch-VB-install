102c1ebdee0e063d396334534c1a18f6507a64ea
podgancar <andrej.premrn@gmail.com>
2015-10-26 18:01:29 +0100

    Added setKeyboard and createPartition.

--- install.sh
+++ install.sh
@@ -1,1 +1,20 @@
  #!/bin/bash
+
+ echo 'Starting custom Arch installation.'
+ echo 'Setting keyboard and updating system clock.'
+ setKeyboard
+ echo 'Making BIOS/MBT partition table and partition.'
+ createPartition
+
+
+ function setKeyboard {
+ 	loadkeys slovene
+ 	setfont lat2-16 -m 8859-2
+ 	# update the system clock
+ 	timedatectl set-ntp true
+ }
+
+ function createPartition {
+ 	(echo o; echo n; echo p; echo 1; echo; echo; echo a; echo w) | fdisk /dev/sda
+ 	mkfs.ext4 /dev/sda1
+ }
